9308d82ad5488e5d26bdb05de6dcdcb4ffa945f0
CKWheatley <88515213+CKWheatley@users.noreply.github.com>
2023-05-02 00:13:32 +0100

    V1.6.1 CV Print Functionality
    Added CV Print functionality so its more maintainable for me.
    
    Currently just a simple design

--- functions.js
+++ functions.js
@@ -323,3 +323,12 @@ function toggle_display(){
     // add more elements to change
 }
 
+
+window.onbeforeprint = function(){
+    function actionPrint(fader){
+        fader.fadeIn(0)
+    }
+    actionPrint($("#experience"));
+    actionPrint($("#profile"));
+  }; // move this to a seperate print module for ease of use.
+  
